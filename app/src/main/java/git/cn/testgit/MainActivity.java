81252fddea89c80a1b38e3dc828ed11b3ca18c8e
sam.tao <sam.tao@yeamoney.cn>
2016-01-02 21:19:57 +0800

    修复点击事件

--- app/src/main/java/git/cn/testgit/MainActivity.java
+++ app/src/main/java/git/cn/testgit/MainActivity.java
@@ -2,16 +2,33 @@ package git.cn.testgit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * 修改文件
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tvTest = (TextView) findViewById(R.id.tvTest);
+        tvTest.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            // 土司
+            case R.id.tvTest:
+                break;
+        }
+    }
 }
